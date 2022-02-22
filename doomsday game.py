9c2651daa62d152ffbca3f6a41b4438e771e449a
MikolajMakedonski <99931214+MikolajMakedonski@users.noreply.github.com>
2022-02-22 22:31:01 +0100

    date format difference bugfix

--- doomsday game.py
+++ doomsday game.py
@@ -11,13 +11,13 @@ def get_input(prompt="",cast=None, condition=None, errorMessage=None):
         except:
             print(errorMessage)
 
-# Difficulty and game number of desired questions choice
+# User settings2
 print("Hi, welcome to The doomsday calculation game!")
 difficulty_level = get_input(prompt="Please choose the number associated with the desired difficulty level:\n1. Begginner (1900-2099)\n2. Intermediate (1800-2199)\n3. Hard (1-9999)\n",
                         cast=int,
                         condition=lambda y: 4>y>0,
                         errorMessage=("Please enter the difficulty 1-3."))
-question_amount = get_input(prompt="How many times do you want to guess?",
+question_amount = get_input(prompt="How many times do you want to guess?\n",
                         cast=int,
                         condition=lambda y: y>0,
                         errorMessage="Please choose a valid number of questions desired.")
@@ -37,16 +37,16 @@ elif difficulty_level == 3:
 dates_span = end_date - start_date
 days_between_dates = dates_span.days
 
-print("Rule: For a random date, you reply with the number of the day in the week (0 being sunday, 1 monday, 2 tuesday etc.)")
+print("Rule: For a random date, you answer what day of the week that is:\n0-Sunday 1-Monday 2-Tuesday 3-Wednesday 4-Thursday 5-Friday 6-Saturday")
 
 # Looping the game number of times chosen by the user before
 x = 0
 user_score = 0
 while x < question_amount:
 
-    # Randomizing between 0 and max number of days in the date span
+    # Getting a random number between 0 and the max days in the chosen difficulty date span
     randomized_number_of_days = random.randrange(0,days_between_dates)
-    # Summing start date with the randomized number of days in the span
+    # Summing start date with the random number of days
     psuedo_random_date = start_date + datetime.timedelta(days = randomized_number_of_days)
 
     print("Your date to guess is",psuedo_random_date.strftime("%d %b, %Y"))
@@ -55,7 +55,8 @@ while x < question_amount:
                         condition= lambda x:-1<x<7,
                         errorMessage="Please enter a valid day number (0 being sunday,1 monday, 2 tuesday etc.)")
 
-    if user_guess == psuedo_random_date.weekday():
+# Adding +1 due to the format difference in .weekday function monday being a 0 instead of sunday as in the original doomsday algorithm and making sure of the exception: answer-0 (sunday) is equal to the program's definiton of sunday-6
+    if user_guess == psuedo_random_date.weekday()+1 or (user_guess==0 and psuedo_random_date.weekday()==6):
        print("You are correct.")
        user_score += 1
     else:
